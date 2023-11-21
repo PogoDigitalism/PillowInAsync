ee0689b41eb4afbb70683dd041e66ad64f7e59a4
Pogo <107322523+PogoDigitalism@users.noreply.github.com>
2023-11-21 22:37:21 +0100

    Update config.py

--- config.py
+++ config.py
@@ -1,17 +1,19 @@
 class Config:
     """
     You will have to tweak these settings a lot until they match with your base template (IMAGE_FILE).
+
+    These standard settings are not configured for the template 'example.png' either.
     """
 
     IMAGE_GEN = {
         'CONSTANTS': {
                     'OFFER': {
-                        'SPACING': 315,
+                        'SPACING': 315, # Horizontal spacing between images
                         'X': 125,
                         'Y': 75,
-                        'ROBUX': (450, 60),
+                        'ROBUX': (450, 60), # (X,Y) tuple
                         'VALUE': (450, 70),
-                        'ROBUX_RGBA': (55, 100, 226, 255),
+                        'ROBUX_RGBA': (55, 100, 226, 255), # (R,G,B,A) text colour tuple
                         'VALUE_RGBA': (255, 255, 255, 255),
                         },        
                     'REQUEST': {
@@ -25,8 +27,8 @@ class Config:
                         },
                       },
 
-        'IMAGE_FILE': 'concept2.png',
-        'SIZE': (1450, 600),
+        'IMAGE_FILE': 'example.png', # serves as a template
+        'SIZE': (1450, 600), # template size
         'FONT': 'segoeuib.ttf',
         'FONT_SIZE': 40,
     }
