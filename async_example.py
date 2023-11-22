c0511d3c3814ef6745cdffd30ca37729f4fba05c
Pogo <107322523+PogoDigitalism@users.noreply.github.com>
2023-11-22 15:04:13 +0100

    Update async_example.py

--- async_example.py
+++ async_example.py
@@ -11,7 +11,8 @@ POOL = ThreadPoolExecutor()
   
 async def task_ImageGeneration(loop):
     result = await loop.run_in_executor(POOL, GenerateImage, ImageConfigs) # Replace GenerateImage and ImageConfigs with your own synchronous Pillow image generator function and configuration
-  
+    # do whatever with 'result' here
+
 async def main():
     loop = asyncio.get_event_loop()
     loop.create_task(task_ImageGeneration(loop)) # create a task if you dont want the execution of main() to be yielded , else just call loop.run_in_executor here directly.
