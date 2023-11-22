b0e2ea345e117708c509641aa77d0c8b6e31d223
Pogo <107322523+PogoDigitalism@users.noreply.github.com>
2023-11-22 13:52:52 +0100

    Create threaded_example.py

---
+++ threaded_example.py
@@ -0,0 +1,19 @@
+#An example of how to multithread synchronous code (Pillow) in an asynchronous environment (for example discord bots with image generation features) using ThreadPoolExecutor.
+import discord
+import asyncio
+from concurrent.futures import ThreadPoolExecutor
+from gen import GenerateImage
+
+def ImageFile() -> discord.File:
+    return GenerateImage({'OFFER': [[sshf_url, sshf_url, pv_url, skotn_url], robux], 'REQUEST': [[cwhp_url], robux]}) # this URL data should be cached by you or something.
+  
+async def TradeAdTask(loop):
+    result = await loop.run_in_executor(ThreadPoolExecutor(), ImageFile)
+    await channel.send(content='Generated Image Test', file=result)
+  
+async def main():
+    loop = asyncio.get_event_loop()
+    loop.create_task(TradeAdTask(loop))
+
+                       
+asyncio.run(main())
