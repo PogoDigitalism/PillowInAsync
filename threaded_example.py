912f15f2c763951a31fde98a988da2276907bb5d
Pogo <107322523+PogoDigitalism@users.noreply.github.com>
2023-11-22 14:43:55 +0100

    Update threaded_example.py

--- threaded_example.py
+++ threaded_example.py
@@ -4,11 +4,13 @@ import asyncio
 from concurrent.futures import ThreadPoolExecutor
 from gen import GenerateImage
 
+POOL = ThreadPoolExecutor()
+
 def ImageFile() -> discord.File:
     return GenerateImage({'OFFER': [[sshf_url, sshf_url, pv_url, skotn_url], robux], 'REQUEST': [[cwhp_url], robux]}) # this URL data should be cached by you or something.
   
 async def TradeAdTask(loop):
-    result = await loop.run_in_executor(ThreadPoolExecutor(), ImageFile)
+    result = await loop.run_in_executor(POOL, ImageFile)
     await channel.send(content='Generated Image Test', file=result)
   
 async def main():
