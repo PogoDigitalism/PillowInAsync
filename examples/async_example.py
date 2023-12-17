f651fe1c6374dd28dd7fe73b53d22b56f4f689f2
Pogo <107322523+PogoDigitalism@users.noreply.github.com>
2023-12-17 14:05:12 +0100

    Update async_example.py

--- examples/async_example.py
+++ examples/async_example.py
@@ -13,7 +13,7 @@ async def task_ImageGeneration(loop):
 
 async def main():
     loop = asyncio.get_event_loop()
-    loop.create_task(task_ImageGeneration(loop)) # create a task if you dont want the execution of main() to be yielded , else just call loop.run_in_executor here directly.
+    loop.create_task(task_ImageGeneration(loop)) # create a task if you dont want the execution of main() to be yielded (asynchronously), else just call loop.run_in_executor here directly.
 
 
 asyncio.run(main())
