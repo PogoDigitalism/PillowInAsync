35ce6cb81008d6c3b54dbbef2e6af6cf4ff2ee0f
Pogo <107322523+PogoDigitalism@users.noreply.github.com>
2024-01-12 12:14:43 +0100

    Update async_example.py
    removed pseudocode

--- examples/async_example.py
+++ examples/async_example.py
@@ -1,18 +1,19 @@
 #An example of how to handle synchronous code (Pillow) in an asynchronous environment using ThreadPoolExecutor.
 
-# !! This example is *PSEUDO-ish CODE*
 import asyncio
 from concurrent.futures import ThreadPoolExecutor
-from gen import GenerateImage
+from assets.gen import GenerateImage
+from assets.config import IMAGE_PARAMS
 
 POOL = ThreadPoolExecutor()
   
 async def task_ImageGeneration(loop):
-    result = await loop.run_in_executor(POOL, GenerateImage, ImageConfigs) # Replace GenerateImage and ImageConfigs with your own synchronous Pillow image generator function and configuration
+    result = await loop.run_in_executor(executor=POOL, func=GenerateImage, IMAGE_PARAMS) # Replace GenerateImage and ImageConfigs with your own synchronous Pillow image generator function and configuration
     # do whatever with 'result' here
 
 async def main():
     loop = asyncio.get_event_loop()
+
     loop.create_task(task_ImageGeneration(loop)) # create a task if you dont want the execution of main() to be yielded (asynchronously), else just call loop.run_in_executor here directly.
 
 
